a08171d52da82b48722dc6235a87cda16fe27218
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-08-24 20:48:37 +0200

    added punctuation and abbreviations to tokenizer class

---
+++ src/Tokenizer.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by cris on 24/08/16.
+  */
+class Tokenizer {
+  val punctuation = ".,;:!?()[]{}`'\"@#$^&*+-|=~_"
+  val abbreviations=List("a.", "adj.", "adv.", "al.", "a.m.", "art.", "c.", "capt.", "cert.", "cf.", "col.", "Col.",
+    "comp.", "conf.", "def",".", "Dep.", "Dept.", "Dr.", "dr.", "ed.", "e.g.", "esp.", "etc.", "ex.",
+  "f.", "fig.", "gen.", "id.", "i.e.", "int.", "l.", "m.", "Med.", "Mil.", "Mr.", "n.", "n.q.",
+  "orig.", "pl.", "pred.", "pres.", "p.m.", "ref.", "v.", "vs.", "w/")
+
+}
