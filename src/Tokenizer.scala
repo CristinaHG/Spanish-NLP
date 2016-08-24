9993b50a43d6309ae0a693ea7ccbef0ae25ff414
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-08-24 21:06:10 +0200

    correcting abbreviations

--- src/Tokenizer.scala
+++ src/Tokenizer.scala
@@ -3,9 +3,13 @@
   */
 class Tokenizer {
   val punctuation = ".,;:!?()[]{}`'\"@#$^&*+-|=~_"
-  val abbreviations=List("a.", "adj.", "adv.", "al.", "a.m.", "art.", "c.", "capt.", "cert.", "cf.", "col.", "Col.",
-    "comp.", "conf.", "def",".", "Dep.", "Dept.", "Dr.", "dr.", "ed.", "e.g.", "esp.", "etc.", "ex.",
-  "f.", "fig.", "gen.", "id.", "i.e.", "int.", "l.", "m.", "Med.", "Mil.", "Mr.", "n.", "n.q.",
-  "orig.", "pl.", "pred.", "pres.", "p.m.", "ref.", "v.", "vs.", "w/")
+  val abbreviations=List("a.C.", "a.m.", "apdo.", "aprox.", "Av.", "Avda.", "c.c.", "D.", "Da.", "d.C.",
+    "d.j.C.", "dna.", "Dr.", "Dra.", "esq.", "etc.", "Gob.", "h.", "m.n.", "no.",
+    "núm.", "pág.", "P.D.", "P.S.", "p.ej.", "p.m.", "Profa.", "q.e.p.d.", "S.A.",
+    "S.L.", "Sr.", "Sra.", "Srta.", "s.s.s.", "tel.", "Ud.", "Vd.", "Uds.", "Vds.",
+    "v.", "vol.", "W.C.")
 
+  val re_abbr1="""^[A-Za-z]\.$""".r // single letter , "T. De Smedt"
+  val re_abbr2="""^([A-Za-z]\.)+$""" //alternating letters, "U.S"
+  val re_abbr3="""^[A-Z][" + "|".join("bcdfghjklmnpqrstvwxz") + "]+.$ """ //# capital followed by consonants, "Mr."
 }
