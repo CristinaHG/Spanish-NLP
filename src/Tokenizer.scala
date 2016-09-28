7d4f77d86ec1f563b08bff439a764b9bc8f140ed
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-09-28 19:22:36 +0200

    ading \ first in emoticon

--- src/Tokenizer.scala
+++ src/Tokenizer.scala
@@ -32,9 +32,10 @@ import scala.util.control.Breaks._
 var re_emoticons=""::Nil
   emoticons.values.foreach(list=>re_emoticons:::=list.flatMap(elem=>"|"::elem::Nil).tail)
   var re1_emoticons=""::Nil
-  re_emoticons.foreach(icon=>if(!(icon.equals("|"))) re1_emoticons::=icon.mkString("?".concat("""\\""").toString) else re1_emoticons::=icon )
+  re_emoticons.foreach(icon=>if(!(icon.equals("|"))) re1_emoticons::=icon.mkString("?".concat("""\\""")) else re1_emoticons::=icon )
 
-  re1_emoticons=re1_emoticons.reverse.tail //important to reserve and to eliminate the first
+  re1_emoticons=re1_emoticons.reverse.tail.map(t => if(!(t.equals("|"))) """\\"""+t else t  ) //important to reserve and to eliminate the first
+
 
   val EOS = "END-OF-SENTENCE"
   var TOKEN="""(\S+)\s""".r
