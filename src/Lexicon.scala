190e18a549659dcb615b1f5fbdc04db3986a80da
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-10-15 20:20:21 +0200

    ading return type in read lexicon

--- src/Lexicon.scala
+++ src/Lexicon.scala
@@ -12,26 +12,27 @@ import scala.xml.Comment
 // Returns an iterator over the lines in the file at the given path,
 //        strippping comments and decoding each line to Unicode.
 
-def read(path:String,encoding:String,comment:String)={
- // var f:Any=""
-  if (!path.isEmpty){
-    if (path.isInstanceOf[String] && Files.exists(Paths.get(path))){
+def read(path:String,encoding:String,comment:String): Unit = {
+  // var f:Any=""
+  if (!path.isEmpty) {
+    if (path.isInstanceOf[String] && Files.exists(Paths.get(path))) {
       //from file path
       var f = scala.io.Source.fromFile(path).getLines()
-      f= f.map(line=> if(f.indexOf(line)==0 && line.isInstanceOf[String]) line.replaceAll("\"+$", "").getBytes(StandardCharsets.UTF_8).mkString else line.getBytes(StandardCharsets.UTF_8).mkString)
-      f=f.filterNot(line=>line.startsWith(comment))
-    }else if(path.isInstanceOf[String]){
-      //from String
-      val f=scala.io.Source.fromString(path)
-    }else{
-      //from file or buffer
-      val f=path
+      f = f.map(line => if (f.indexOf(line) == 0 && line.isInstanceOf[String]) line.replaceAll("\"+$", "").getBytes(StandardCharsets.UTF_8).mkString else line.getBytes(StandardCharsets.UTF_8).mkString)
+      f = f.filterNot(line => line.startsWith(comment))
+      return f
+      //    }else if(path.isInstanceOf[String]){
+      //      //from String
+      //      val f=scala.io.Source.fromString(path)
+      //    }else{
+      //      //from file or buffer
+      //      val f=path
+      //    }
     }
-  }else throw new IllegalArgumentException("a path must be specified")
+  } else throw new IllegalArgumentException("a path must be specified")
 }
 
-
-class Lexicon {
-
-
-}
+//class Lexicon {
+//
+//
+//}
