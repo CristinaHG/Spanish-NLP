915b86afbd22d1c81debb1513903d55cefed1523
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-10-17 21:38:53 +0200

    creating intern dictionary

--- src/Lexicon.scala
+++ src/Lexicon.scala
@@ -4,6 +4,7 @@ import java.nio.file.{Files, Paths}
 
 import com.sun.media.jfxmedia.track.Track.Encoding
 
+import scala.collection.mutable
 import scala.io.Source
 import scala.xml.Comment
 
@@ -16,15 +17,20 @@ import scala.xml.Comment
 class Lexicon {
 
 
-  def read(path: String, encoding: String, comment: String): Unit = {
-    // var f:Any=""
+  def read(path: String, encoding: String, comment: String): mutable.Map[String, String] = {
+    //creating empty lexicon list
+    val emoticons= mutable.Map[String,String]()
     if (!path.isEmpty) {
       if (path.isInstanceOf[String] && Files.exists(Paths.get(path)) ) {
         //from file path
-        var f = scala.io.Source.fromFile(path).getLines().filter(line=>(!line.startsWith(comment)))
-       // f = f.map(line => if (f.indexOf(line) == 0 && line.isInstanceOf[String]) line.replaceAll("\"+$", "").getBytes(StandardCharsets.UTF_8).mkString else line.getBytes(StandardCharsets.UTF_8).mkString)
+        var f = scala.io.Source.fromFile(path).getLines().filter(line=>(!line.startsWith(comment))).map{line=>
+          val x=line.split(" ")
+          (x.head,x.last)}.foreach(u=>emoticons+=(u._1->u._2))
+        //.foreach(s=> emoticons+=(s(0)->s(1)))
+       // f = f.map(line => if (f.indexOf(line) == 0 && line.isInstanceOf[String]) line.replaceAll("\"+$", "") else line)
+        //f.foreach(line=> line.split(" ").foreach(s=> emoticons+=(s(0)->s(1))))
        // f = f.filterNot(line => line.startsWith(comment))
-        return f
+        return emoticons
         //    }else if(path.isInstanceOf[String]){
         //      //from String
         //      val f=scala.io.Source.fromString(path)
