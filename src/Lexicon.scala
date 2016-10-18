1ba3ed662b551376e2d13ef7f389ea2eeb6f9d39
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-10-18 22:13:42 +0200

    adding exception and reading cases in read method

--- src/Lexicon.scala
+++ src/Lexicon.scala
@@ -6,41 +6,41 @@ import com.sun.media.jfxmedia.track.Track.Encoding
 
 import scala.collection.mutable
 import scala.io.Source
+import scala.reflect.io.File
 import scala.xml.Comment
 
 /**
   * Created by cris on 12/10/16.
   */
-// Returns an iterator over the lines in the file at the given path,
-//        strippping comments and decoding each line to Unicode.
 
 class Lexicon {
 
-
+  // Returns a dictironary with readed lexicon over the lines in the file at the given path strippping comments
   def read(path: String, encoding: String, comment: String): mutable.Map[String, String] = {
-    //creating empty lexicon list
-    val emoticons= mutable.Map[String,String]()
+    //creating empty lexicon dict
+    val lexiconDict= mutable.Map[String,String]()
+
     if (!path.isEmpty) {
       if (path.isInstanceOf[String] && Files.exists(Paths.get(path)) ) {
         //from file path
-        var f = scala.io.Source.fromFile(path).getLines().filter(line=>(!line.startsWith(comment))).map{line=>
+        val f = scala.io.Source.fromFile(path).getLines().filter(line=>(!line.startsWith(comment))).map{line=>
           val x=line.split(" ")
-          (x.head,x.last)}.foreach(u=>emoticons+=(u._1->u._2))
-        //.foreach(s=> emoticons+=(s(0)->s(1)))
+          (x.head,x.last)}.foreach(u=>lexiconDict+=(u._1->u._2))
+        //replace "\" by "" -> codecs.BOMUTF8 not necesary because of codification
        // f = f.map(line => if (f.indexOf(line) == 0 && line.isInstanceOf[String]) line.replaceAll("\"+$", "") else line)
-        //f.foreach(line=> line.split(" ").foreach(s=> emoticons+=(s(0)->s(1))))
-       // f = f.filterNot(line => line.startsWith(comment))
-
-        //    }else if(path.isInstanceOf[String]){
-        //      //from String
-        //      val f=scala.io.Source.fromString(path)
-        //    }else{
-        //      //from file or buffer
-        //      val f=path
-        //    }
-      }
-    } //else throw new IllegalArgumentException("a path must be specified")
-    return emoticons
+      }else if(path.isInstanceOf[String]){
+        //from String
+              val f=scala.io.Source.fromString(path).getLines().filter(line=>(!line.startsWith(comment))).map{line=>
+                val x=line.split(" ")
+                (x.head,x.last)}.foreach(u=>lexiconDict+=(u._1->u._2))
+      }else{
+        //from file or buffer
+              val f=scala.io.Source.fromBytes(path.toBuffer.toArray,encoding).getLines().filter(line=>(!line.startsWith(comment))).map{line=>
+          val x=line.split(" ")
+          (x.head,x.last)}.foreach(u=>lexiconDict+=(u._1->u._2))
+            }
+      }else throw new IllegalArgumentException("a path must be specified")
+    return lexiconDict
   }
 
 }
