1383cb776c4068c27685fec0ebeb91d0819c9bc6
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-10-22 17:45:45 +0200

    adding rulesSet to Morphology class

--- src/Morphology.scala
+++ src/Morphology.scala
@@ -1,5 +1,6 @@
 import java.nio.file.{Files, Paths}
 
+import scala.collection.JavaConverters._
 import scala.collection.mutable
 
 /**
@@ -12,6 +13,21 @@ import scala.collection.mutable
 //     ly hassuf 2 RB x => unknown words ending in -ly change to RB.
 class Morphology {
 
+  //set of rules, based on word morphology (prefix,suffix)
+  val rulesSet= Set(
+    "word", // Word is x.
+    "char", // Word contains x.
+    "haspref", // Word starts with x.
+    "hassuf", // Word end with x.
+    "addpref", // x + word is in lexicon.
+    "addsuf", // Word + x is in lexicon.
+    "deletepref", // Word without x at the start is in lexicon.
+    "deletesuf", // Word without x at the end is in lexicon.
+    "goodleft", // Word preceded by word x.
+    "goodright"// Word followed by word x.
+  )
+
+  
   // Returns a list of lists as the result of readed morphology.txt rules
   def read(path: String, encoding: String, comment: String): List[List[String]] = {
     var morphology:List[List[String]]=Nil
@@ -33,7 +49,7 @@ class Morphology {
     return morphology
   }
 //Applies lexical rules to the given token, which is a [word, tag] list.
-  def apply(token:String,previus:(String,String), next:(String,String)): Unit ={
+  def apply(token:String,previus:(String,String), next:(String,String),morphology:List[List[String]]): Unit ={
 
   }
 }
