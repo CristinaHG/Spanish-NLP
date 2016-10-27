5da604d4afff10dec959ddd954ae12dc4aa83e33
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-10-27 20:18:44 +0200

    implementing apply method

--- src/Context.scala
+++ src/Context.scala
@@ -60,5 +60,51 @@ class Context {
     this.contextList=context.reverse
     //return morphology
   }
+
+  //Applies contextual rules to the given list of tokens, where each token is a [word,tag] list.
+  def apply(tokensTags:List[(String,String)])={
+    val o=List(("STAART", "STAART"),("STAART", "STAART"),("STAART", "STAART")) //empty delimiters for look ahead/back
+    var t=o.++:(tokensTags).++(o)
+    t.map(token=> this.contextList.foreach(r=>
+        if(!((token._2=="STAART") || (token._2!=r(0) && r(0)!="*"))){
+          val cmd=r(2).toLowerCase
+          val x=r(3)
+          val y=if(r.length>4) r(4) else ""
+
+          if((cmd=="prevtag" && x==t(t.indexOf(token)-1)._2) ||
+          (cmd=="nexttag" && x==t(t.indexOf(token)+1)._2) ||
+            (cmd=="prev2tag" && x==t(t.indexOf(token)-2)._2) ||
+            (cmd=="next2tag" && x==t(t.indexOf(token)+2)._2) ||
+            (cmd=="prev1or2tag" && (  (t(t.indexOf(token)-1)._2, t(t.indexOf(token)-2)._2).toString().contains(x) )) ||
+            (cmd=="next1or2tag" && (  (t(t.indexOf(token)+1)._2, t(t.indexOf(token)+2)._2).toString().contains(x) )) ||
+            (cmd=="prev1or2or3tag" && ( (t(t.indexOf(token)-1)._2, t(t.indexOf(token)-2)._2,t(t.indexOf(token)-3)._2).toString().contains(x) )) ||
+            (cmd=="next1or2or3tag" && ( (t(t.indexOf(token)-1)._2, t(t.indexOf(token)-2)._2,t(t.indexOf(token)-3)._2).toString().contains(x) )) ||
+            (cmd=="surroundtag" && ( x==t(t.indexOf(token)-1)._2 && y==t(t.indexOf(token)+1)._2 )) ||
+            (cmd=="curwd" && ( x==t(t.indexOf(token))._1 )) ||
+            (cmd=="prevwd" && (x==t(t.indexOf(token)-1)._1 )) ||
+            (cmd=="nextwd" && (x==t(t.indexOf(token)+1)._1 )) ||
+            (cmd=="prev1or2wd" && (t(t.indexOf(token)-1)._1,t(t.indexOf(token)-2)._1).toString().contains(x)) ||
+            (cmd=="next1or2wd" && (t(t.indexOf(token)+1)._1,t(t.indexOf(token)+2)._1).toString().contains(x)) ||
+            (cmd=="prevwdtag" && (x==t(t.indexOf(token)-1)._1 && y==t(t.indexOf(token)-1)._2 )) ||
+            (cmd=="nextwdtag" && (x==t(t.indexOf(token)+1)._1 && y==t(t.indexOf(token)+1)._2 )) ||
+            (cmd=="wdprevtag" && (x==t(t.indexOf(token)-1)._2 && y==t(t.indexOf(token))._1 )) ||
+            (cmd=="wdnexttag" && (x==t(t.indexOf(token))._1 && y==t(t.indexOf(token)+1)._2 )) ||
+            (cmd=="wdand2aft" && (x==t(t.indexOf(token))._1 && y==t(t.indexOf(token)+2)._1 )) ||
+            (cmd=="wdand2tagbfr" && (x==t(t.indexOf(token)-2)._2 && y==t(t.indexOf(token))._1 )) ||
+            (cmd=="wdand2tagaft" && (x==t(t.indexOf(token))._1 && y==t(t.indexOf(token)+2)._2 )) ||
+            (cmd=="lbigram" && (x==t(t.indexOf(token)-1)._1 && y==t(t.indexOf(token))._1 )) ||
+            (cmd=="rbigram" && (x==t(t.indexOf(token))._1 && y==t(t.indexOf(token)+1)._1 )) ||
+            (cmd=="prevbigram" && (x==t(t.indexOf(token)-2)._2 && y==t(t.indexOf(token)-1)._2 )) ||
+            (cmd=="nextbigram" && (x==t(t.indexOf(token)+1)._2 && y==t(t.indexOf(token)+2)._2 ))
+          ) (t(t.indexOf(token))._1,r(1))
+        }))
+      return t
+
+
+  }
+
+
+
+
 }
 
