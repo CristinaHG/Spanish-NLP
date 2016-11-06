fe5eb5b043f0f9b48fc48eb28ce9ca27c82c953f
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-11-06 22:25:19 +0100

    correcting problem with indexOf

--- src/Context.scala
+++ src/Context.scala
@@ -75,6 +75,7 @@ class Context {
     var y=""
     var r1=""
     var matches=false
+    var index=0
 
    t.foreach(token=> { this.contextList.foreach(r=>
      if ((token._2 != "STAART") && (token._2 == r(0) || r(0) == "*")) {
@@ -82,34 +83,35 @@ class Context {
        x = r(3)
        y = if (r.length > 4) r(4) else ""
 
-       if ((cmd == "prevtag" && x == t(t.indexOf(token) - 1)._2) ||
-         (cmd == "nexttag" && x == t(t.indexOf(token) + 1)._2) ||
-         (cmd == "prev2tag" && x == t(t.indexOf(token) - 2)._2) ||
-         (cmd == "next2tag" && x == t(t.indexOf(token) + 2)._2) ||
-         (cmd == "prev1or2tag" && ((t(t.indexOf(token) - 1)._2, t(t.indexOf(token) - 2)._2).toString().contains(x))) ||
-         (cmd == "next1or2tag" && ((t(t.indexOf(token) + 1)._2, t(t.indexOf(token) + 2)._2).toString().contains(x))) ||
-         (cmd == "prev1or2or3tag" && ((t(t.indexOf(token) - 1)._2, t(t.indexOf(token) - 2)._2, t(t.indexOf(token) - 3)._2).toString().contains(x))) ||
-         (cmd == "next1or2or3tag" && ((t(t.indexOf(token) + 1)._2, t(t.indexOf(token) + 2)._2, t(t.indexOf(token) + 3)._2).toString().contains(x))) ||
-         (cmd == "surroundtag" && (x == t(t.indexOf(token) - 1)._2 && y == t(t.indexOf(token) + 1)._2)) ||
-         (cmd == "curwd" && (x == t(t.indexOf(token))._1)) ||
-         (cmd == "prevwd" && (x == t(t.indexOf(token) - 1)._1)) ||
-         (cmd == "nextwd" && (x == t(t.indexOf(token) + 1)._1)) ||
-         (cmd == "prev1or2wd" && (t(t.indexOf(token) - 1)._1, t(t.indexOf(token) - 2)._1).toString().contains(x)) ||
-         (cmd == "next1or2wd" && (t(t.indexOf(token) + 1)._1, t(t.indexOf(token) + 2)._1).toString().contains(x)) ||
-         (cmd == "prevwdtag" && (x == t(t.indexOf(token) - 1)._1 && y == t(t.indexOf(token) - 1)._2)) ||
-         (cmd == "nextwdtag" && (x == t(t.indexOf(token) + 1)._1 && y == t(t.indexOf(token) + 1)._2)) ||
-         (cmd == "wdprevtag" && (x == t(t.indexOf(token) - 1)._2 && y == t(t.indexOf(token))._1)) ||
-         (cmd == "wdnexttag" && (x == t(t.indexOf(token))._1 && y == t(t.indexOf(token) + 1)._2)) ||
-         (cmd == "wdand2aft" && (x == t(t.indexOf(token))._1 && y == t(t.indexOf(token) + 2)._1)) ||
-         (cmd == "wdand2tagbfr" && (x == t(t.indexOf(token) - 2)._2 && y == t(t.indexOf(token))._1)) ||
-         (cmd == "wdand2tagaft" && (x == t(t.indexOf(token))._1 && y == t(t.indexOf(token) + 2)._2)) ||
-         (cmd == "lbigram" && (x == t(t.indexOf(token) - 1)._1 && y == t(t.indexOf(token))._1)) ||
-         (cmd == "rbigram" && (x == t(t.indexOf(token))._1 && y == t(t.indexOf(token) + 1)._1)) ||
-         (cmd == "prevbigram" && (x == t(t.indexOf(token) - 2)._2 && y == t(t.indexOf(token) - 1)._2)) ||
-         (cmd == "nextbigram" && (x == t(t.indexOf(token) + 1)._2 && y == t(t.indexOf(token) + 2)._2))
+       if ((cmd == "prevtag" && x == t(index - 1)._2) ||
+         (cmd == "nexttag" && x == t(index + 1)._2) ||
+         (cmd == "prev2tag" && x == t(index - 2)._2) ||
+         (cmd == "next2tag" && x == t(index + 2)._2) ||
+         (cmd == "prev1or2tag" && ((t(index - 1)._2, t(index - 2)._2).toString().contains(x))) ||
+         (cmd == "next1or2tag" && ((t(index + 1)._2, t(index + 2)._2).toString().contains(x))) ||
+         (cmd == "prev1or2or3tag" && ((t(index - 1)._2, t(index - 2)._2, t(index - 3)._2).toString().contains(x))) ||
+         (cmd == "next1or2or3tag" && ((t(index + 1)._2, t(index + 2)._2, t(index + 3)._2).toString().contains(x))) ||
+         (cmd == "surroundtag" && (x == t(index - 1)._2 && y == t(index + 1)._2)) ||
+         (cmd == "curwd" && (x == t(index)._1)) ||
+         (cmd == "prevwd" && (x == t(index - 1)._1)) ||
+         (cmd == "nextwd" && (x == t(index + 1)._1)) ||
+         (cmd == "prev1or2wd" && (t(index - 1)._1, t(index - 2)._1).toString().contains(x)) ||
+         (cmd == "next1or2wd" && (t(index + 1)._1, t(index + 2)._1).toString().contains(x)) ||
+         (cmd == "prevwdtag" && (x == t(index - 1)._1 && y == t(index - 1)._2)) ||
+         (cmd == "nextwdtag" && (x == t(index + 1)._1 && y == t(index + 1)._2)) ||
+         (cmd == "wdprevtag" && (x == t(index - 1)._2 && y == t(index)._1)) ||
+         (cmd == "wdnexttag" && (x == t(index)._1 && y == t(index + 1)._2)) ||
+         (cmd == "wdand2aft" && (x == t(index)._1 && y == t(index + 2)._1)) ||
+         (cmd == "wdand2tagbfr" && (x == t(index - 2)._2 && y == t(index)._1)) ||
+         (cmd == "wdand2tagaft" && (x == t(index)._1 && y == t(index + 2)._2)) ||
+         (cmd == "lbigram" && (x == t(index - 1)._1 && y == t(index)._1)) ||
+         (cmd == "rbigram" && (x == t(index)._1 && y == t(index + 1)._1)) ||
+         (cmd == "prevbigram" && (x == t(index - 2)._2 && y == t(index - 1)._2)) ||
+         (cmd == "nextbigram" && (x == t(index + 1)._2 && y == t(index + 2)._2))
        ) {matches=true ;r1=r(1)}
      })
    if(matches){ mapped+=Tuple2(token._1,r1) ; matches=false} else mapped+=token
+   index+=1
    })
     return mapped.filter(p=>p!="STAART").toList
   }
